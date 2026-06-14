c528a6e055a171703590433b155513ed17d0b7b6
GustavoACR <125524220+GustavoACR@users.noreply.github.com>
2026-06-13 19:23:39 -0700

    hola

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,20 @@
+# suma y reta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+#multiplicacion y division (* y /)
+multi = 12 * 5
+division = 12 / 5 #devuelde un flotante
+
+#potenciacion (exponente) (**)
+exponente = 9**2
+
+#division baja (//)
+division_baja = 12//5 #devuelve entero redondeado hacia abajo
+
+#resto o modulo 
+resto = 12 % 5
+
+tipo_de_dato = type(5.3) #nos devuelve el tipo de dato
+
+print(resto)
