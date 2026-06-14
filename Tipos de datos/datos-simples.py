0280eea1a6389447f6cea3e0e63be86c6a22b5bd
GustavoACR <125524220+GustavoACR@users.noreply.github.com>
2026-06-13 18:14:23 -0700

    hola

--- Tipos de datos/datos-simples.py
+++ Tipos de datos/datos-simples.py
@@ -1,3 +1,7 @@
-nombre = "lucas"
-nombre = "Dalto"
-print("hola mundo") 
+"string" #texto
+40 #enteros
+40.5 #doubles
+
+#booleanos
+False
+True
---
+++ variables/variables.py
@@ -0,0 +1,23 @@
+#definicion de variable
+nombre = 5
+numero = 10
+numero +=5
+
+#definicion de una variable con camelCase
+nomobreCompleto = "jose jose"
+
+#definicion de una variable con snake_case
+nombre_completo = "jose jose"
+
+#concatenar con +
+# bienvenida = "hola " + nombre + " como estas?"
+
+#concatenar con f-string
+bienvenida = f"hola {nombre} como estas?"
+
+# del bienvenida
+# print(bienvenida) 
+
+#operadores de pertenencia in / not in
+print("jose" not in bienvenida)
+print("ola" in bienvenida)
